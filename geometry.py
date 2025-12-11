67a31f61440ad826726528fe5e2d0a83de6202cb
Sanika Patil <patilsanika1912@gmail.com>
2025-12-11 06:07:23 +0530

    rectangle

---
+++ geometry.py
@@ -0,0 +1,4 @@
+def calc_area(len,br):
+    print("Area : ",(len*br))
+def calc_perimeter(len,br):
+    print("Area : ",(2*(len+br)))
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import arithmetic
+import geometry
 a=int(input("Enter num1 :"))
 b=int(input("Enter num2 :"))
 arithmetic.add(a,b)
 arithmetic.subtract(a,b)
+len=int(input("Enter length :"))
+br=int(input("Enter breadth :"))
+geometry.calc_area(len,br)
+geometry.calc_perimeter(len,br)
