30e2c24a0cb7b5e0fc6f2095314dcd45372eb50a
Sanika Patil <patilsanika1912@gmail.com>
2025-12-11 05:57:24 +0530

    arithmetic operations

---
+++ arithmetic.py
@@ -0,0 +1,4 @@
+def add(num1,num2):
+    print("a + b = ",num1+num2)
+def subtract(num1,num2):
+    print("a - b = ",num1-num2)
--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print("Hello ,World")
+import arithmetic
+a=int(input("Enter num1 :"))
+b=int(input("Enter num2 :"))
+arithmetic.add(a,b)
+arithmetic.subtract(a,b)
